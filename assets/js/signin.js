61bb1b817f538c9168376718f0dc6f2bca4832ef
tiagofcc <tiagofccosta@ua.pt>
2024-12-27 16:37:59 +0000

    1234

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -2,7 +2,7 @@ if (!localStorage.getItem("joao@gmail.com")) {
     var d = {Nome:"joao",
         Id:0,
         Email:"joao@gmail.com",
-        Password:"aaaa",
+        Password:"qqqq",
         Pagamento:[]
     }
     localStorage.setItem("joao@gmail.com",JSON.stringify(d));
@@ -13,7 +13,7 @@ function signin () {
     var nome = document.getElementById('nome');
     var email = document.getElementById('email');
     var password = document.getElementById('password');
-    var confpass = document.getElementById('confpassword');
+    var password2 = document.getElementById('password2');
 
     var id_uni = localStorage.getItem("useriduniversal");
 
@@ -23,28 +23,27 @@ function signin () {
         var id = 1;
     }
     
-    if (nome.value.trim().length < 3) {
-        alert("O nome tem de ter no mínimo três letras");
+    if (nome.value.trim().length < 3 || /[^a-zA-Z\s]/.test(nome.value)) {
+        alert("O nome deve ter no mínimo três letras e não pode conter números ou caracteres especiais.");
     } else if (!/^.{3,}@.{3,}\..{2,}$/.test(email.value.trim())) {
         alert("Por favor insira o email");
     } else if (password.value.length < 4) {
         alert("A password tem de ter no mínimo quatro caracteres!");
-    } else if (confpass.value.trim() !== password.value.trim()) {
+    } else if (password2.value.trim() !== password.value.trim()) {
         alert("A password não é igual nos dois campos!");
     } else {
         conta = {
-            Nome: nome.value,
             Id: id,
+            Nome: nome.value,
             Email: email.value,
             Password: password.value,
-            Cargo: "user",
             Pagamento: []
         };
 
         localStorage.setItem("useriduniversal", JSON.stringify(id+1))
         localStorage.setItem(email.value, JSON.stringify(conta));
         localStorage.setItem("atual", JSON.stringify(conta));
-        window.location.href = 'perfil.html';
+        window.location.href = 'index.html';
     }
 }
 
